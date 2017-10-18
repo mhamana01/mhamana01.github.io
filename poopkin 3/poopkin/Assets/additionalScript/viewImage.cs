7e4d43a93deac969a6a440582441cf24e440a6aa
mhamana01 <maricolton@gmail.com>
2017-10-18 20:49:49 +0900

    update

---
+++ poopkin 3/poopkin/Assets/additionalScript/viewImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class viewImage : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    public Sprite img;
+
+	// Use this for initialization
+	void Start () {
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = img;
+            
+        //Sprite spriteImage = Resources.Load("poop.png", typeof(Sprite)) as Sprite;
+        //new GameObject("Sprite").AddComponent<SpriteRenderer>().sprite = spriteImage;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+
+    }
+}
