940de6aa39c19d34c96b2b46817ba5550e8a5b01
mhamana01 <maricolton@gmail.com>
2017-10-22 12:30:46 +0900

    c

---
+++ poopkin/Assets/additionalScript/Character.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Windows.Kinect;
+using Microsoft.Kinect.VisualGestureBuilder;
+
+public class Character : MonoBehaviour
+{
+    public string databaseName = "poop_1.gbd";
+    public BodySourceManager bodyManager;
+    VisualGestureBuilderFrameSource gestureSource;
+    VisualGestureBuilderFrameReader gestureReader;
+    KinectSensor sensor = null;
+    public Dictionary<Gesture, DiscreteGestureResult> DiscreteGestureResults;
+
+    SpriteRenderer MainSpriteRenderer;
+
+    public Sprite StandbySprite;
+    public Sprite HoldSprite;
+
+
+    void Start()
+    {
+        sensor = KinectSensor.GetDefault();
+        if (sensor != null)
+        {
+            if (!sensor.IsOpen)
+            {
+                Debug.Log("is OPEN");
+                sensor.Open();
+            }
+        }
+        gestureSource = VisualGestureBuilderFrameSource.Create(sensor, 0);
+        gestureReader = gestureSource.OpenReader();
+        if (gestureReader != null)
+        {
+            gestureReader.IsPaused = true;
+            gestureReader.FrameArrived += GestureFrameArrived;
+            //Debug.Log("IS PAUSED");
+        }
+
+        string path = System.IO.Path.Combine(Application.streamingAssetsPath, databaseName);
+        using (VisualGestureBuilderDatabase database = VisualGestureBuilderDatabase.Create(path))
+        {
+            foreach (Gesture gesture in database.AvailableGestures)
+            {
+                gestureSource.AddGesture(gesture);
+                //Debug.Log(gesture.Name);
+            }
+        }
+
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!gestureSource.IsTrackingIdValid)
+        {
+            FindValidBody();
+        }
+    }
+
+    void FindValidBody()
+    {
+        if (bodyManager != null)
+        {
+            Body[] bodies = bodyManager.GetData();
+            if (bodies != null)
+            {
+                foreach (Body body in bodies)
+                {
+                    if (body.IsTracked)
+                    {
+                        SetBody(body.TrackingId);
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void SetBody(ulong id)
+    {
+        if (id > 0)
+        {
+            gestureSource.TrackingId = id;
+            gestureReader.IsPaused = false;
+        }
+        else
+        {
+            gestureSource.TrackingId = 0;
+            gestureReader.IsPaused = true;
+        }
+    }
+
+    void GestureFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
+    {
+        using (var frame = e.FrameReference.AcquireFrame())
+        {
+            if (frame != null)
+            {
+                DiscreteGestureResults = frame.DiscreteGestureResults;
+                //Debug.Log("FRAME ARRIVED");
+
+                if (DiscreteGestureResults != null)
+                {
+                    foreach (KeyValuePair<Gesture, DiscreteGestureResult> pair in DiscreteGestureResults)
+                    {
+                        //Debug.Log(pair.Key.Name);
+                        Debug.Log("detected: " + pair.Value.Detected + ", confidence: " + pair.Value.Confidence);
+
+                        if (pair.Value.Detected == true)
+                        {
+               
+                            MainSpriteRenderer.sprite = HoldSprite; 
+                        }
+                        else
+                        {
+                           MainSpriteRenderer.sprite = StandbySprite;
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
